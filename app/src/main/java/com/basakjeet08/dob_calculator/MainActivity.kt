9408b39f16af987fc86d77b1d1a6e9a0d96a6336
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-02 02:13:35 +0530

    02/10/2022

--- app/src/main/java/com/basakjeet08/dob_calculator/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/dob_calculator/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.basakjeet08.dob_calculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker:Button = findViewById(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+
+    }
+
+    private fun clickDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view , year , month , dayOfMonth ->},
+            year,month,day
+        ).show()
+        Toast.makeText(this,"Date Picker Pressed " , Toast.LENGTH_LONG).show()
     }
 }
