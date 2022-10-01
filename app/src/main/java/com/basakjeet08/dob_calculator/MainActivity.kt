9cb54cd0ce9cab715055872bcd0eac055b852a95
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-02 02:58:32 +0530

    02/10/2022

--- app/src/main/java/com/basakjeet08/dob_calculator/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/dob_calculator/MainActivity.kt
@@ -4,8 +4,8 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.Toast
+import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,20 +14,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnDatePicker:Button = findViewById(R.id.btnDatePicker)
+        val myCalendar = Calendar.getInstance()
+        val tvSelectedDate : TextView = findViewById(R.id.tvSelectedDate)
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        tvSelectedDate.text = "$day/${month+1}/$year"
+
+
+
         btnDatePicker.setOnClickListener {
-            clickDatePicker()
+            clickDatePicker(day,month,year,tvSelectedDate)
         }
 
     }
 
-    private fun clickDatePicker(){
-        val myCalendar = Calendar.getInstance()
-        val year = myCalendar.get(Calendar.YEAR)
-        val month = myCalendar.get(Calendar.MONTH)
-        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view , year , month , dayOfMonth ->},
+    private fun clickDatePicker(day:Int , month:Int , year:Int , tvSelectedDate : TextView){
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view , selectedYear , selectedMonth , dayOfMonth ->
+            //Toast.makeText(this,"$dayOfMonth/${selectedMonth+1}/$selectedYear" , Toast.LENGTH_LONG).show()
+            tvSelectedDate.text = "$dayOfMonth/${selectedMonth+1}/$selectedYear"
+
+            val todayDate = year*365 + month*30 + day
+            val bodDate = selectedYear*365 + selectedMonth*30 + dayOfMonth
+            val differenceInDays = todayDate - bodDate
+
+
+        },
             year,month,day
         ).show()
-        Toast.makeText(this,"Date Picker Pressed " , Toast.LENGTH_LONG).show()
+
+        Toast.makeText(this,"$day/${month+1}/$year" , Toast.LENGTH_LONG).show()
     }
 }
